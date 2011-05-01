97a5e5a49416f9709dd0797c9b826116be383709
Gabriel Alberto Gimenez <gabrielgimenez85@gmail.com>
2011-04-30 22:21:06 -0400

    tabla Usuario creada

---
+++ PROYECTO-SAP-TG/proyectosaptg/model/usuario.py
@@ -0,0 +1,22 @@
+from sqlalchemy import *
+from sqlalchemy.orm import mapper, relation, backref
+from sqlalchemy.types import *
+
+from proyectosaptg.model import DeclarativeBase, metadata, DBSession
+
+__all__ = [ 'Usuario' ]
+
+class Usuario(DeclarativeBase):
+
+    __tablename__ = 'usuario'
+
+    id_usuario = Column(Integer, primary_key=True)
+    username = Column(Unicode, nullable=False)
+    password = Column(Unicode, nullable=False)
+    fecha_creacion = Column(Date, nullable=False)
+
+    def __repr__(self):
+        return (u"<Usuario('%s','%s', '%s', '%s')>" % (
+            self.id_usuario, self.username, self.password, self.fecha_creacion
+        )).encode('utf-8')
+
